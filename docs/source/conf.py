7dc69dd2dafdad2646e54d63fbe6f6465cd677b9
nadjano <nnolte@ebi.ac.uk>
2025-11-18 14:56:57 +0100

    fix: update project name to LongPolyASE in Sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'polyase'
+project = 'LongPolyASE'
 copyright = '2025, Nadja Nolte'
 author = 'Nadja Nolte'
 release = '0.1.0'
